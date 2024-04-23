f5921794b32d9d28aaf535c69fd9f2096fc26e65
Smykovsky <kamil.smyk00@gmail.com>
2024-04-23 15:28:16 +0200

    fix

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.getElementById("fadeInText").innerText;
-    const letters = text.split("");
-    document.getElementById("fadeInText").innerHTML = "";
+async function loadHTML(url) {
+    var xhr = new XMLHttpRequest();
 
-    letters.forEach(function(letter, index) {
-        const span = document.createElement("span");
-        span.innerText = letter;
-        span.style.animationDelay = `${index * 0.1}s`;
-        document.getElementById("fadeInText").appendChild(span);
-    });
+    await xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            // Wstaw załadowany HTML do elementu o id "content"
+            document.getElementById('content').innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.getElementById("fadeIn");
+    const textValue = "Full Stack Developer";
+    let index = 0;
+    let intervalId;
+
+function addLetter() {
+    if (index < textValue.length) {
+        textElement.textContent += textValue[index];
+        index++;
+    } else {
+        clearInterval(intervalId);
+        setTimeout(startTyping, 2000);
+    }
+}
+
+function startTyping() {
+    textElement.textContent = '';
+    index = 0;
+    intervalId = setInterval(addLetter, 100);
+}
+
+startTyping();
 });
