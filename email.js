c0a5c32c7ddb26bb3150fcdf9a3aec48931dd1fa
smyku <kamil.smyk00@gmail.com>
2024-07-01 12:56:31 +0200

    f

--- email.js
+++ email.js
@@ -31,8 +31,7 @@ function sendMail() {
       document.getElementById("subject").value = "";
       document.getElementById("message").value = "";
       console.log(res);
-      alert("Your message sent successfully!!")
-
+      alert("Your message sent successfully!!");
     })
     .catch(err => console.log(err));
 }
