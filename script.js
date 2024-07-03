eb2a84f904290c753b99d0cd0cacb8285f342e6f
Smykovsky <kamil.smyk00@gmail.com>
2024-07-03 12:17:53 +0200

    fix

--- script.js
+++ script.js
@@ -12,23 +12,34 @@ async function loadHTML(url) {
     xhr.send();
 }
 
-function setActiveNavLink(url) {
-    const navLinks = document.querySelectorAll('.nav li');
-    navLinks.forEach(link => link.classList.remove('active'));
-
-    // Ustawienie aktywnej klasy dla odpowiedniego linku na podstawie URL
-    const activeLink = document.getElementById(getLinkIdFromUrl(url));
-    if (activeLink) {
-        activeLink.classList.add('active');
-    }
-}
 
 function changeStyle(elementName) {
     const allLiElements = document.querySelectorAll(".nav li");
     allLiElements.forEach(e => e.classList.remove("active"));
     const liElement = document.getElementById(elementName);
+    const mobileLiElement = document.getElementById(elementName + "-mobile");
+    mobileLiElement.classList.add("active")
     liElement.classList.add("active");
+}
 
+function openMenu() {
+    var menu = document.getElementById('nav-mobile');
+    var toggleButtonOn = document.getElementById("toggleButtonOn");
+    var toggleButtonOff = document.getElementById("toggleButtonOff");
+
+    menu.style.display = 'flex';
+    toggleButtonOn.style.display = 'block';
+    toggleButtonOff.style.display = 'none';
+}
+
+function hideMenu() {
+    var menu = document.getElementById('nav-mobile');
+    var toggleButtonOn = document.getElementById("toggleButtonOn");
+    var toggleButtonOff = document.getElementById("toggleButtonOff");
+
+    menu.style.display = 'none';
+    toggleButtonOn.style.display = 'none';
+    toggleButtonOff.style.display = 'block';
 }
 
 
